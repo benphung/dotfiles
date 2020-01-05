69ffc4ec91fa62c720caf05fcf96427d65d33751
Ben Phung <benphungg@gmail.com>
2020-01-05 10:33:25 -0800

    Add Homebrew

---
+++ brew/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Updating homebrew..."
+brew update
+
+echo "Installing packages..."
+IFS=$'\n' PACKAGES=($(<leaves))
+brew install ${PACKAGES[@]}
+
+echo "Installing casks..."
+IFS=$'\n' CASKS=($(<casks))
+brew cask install ${CASKS[@]}
+
+echo "Upgrading packages..."
+brew upgrade
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Doctoring..."
+brew doctor
