3b81100517b17f7467bf3a886375e2b796880dad
Ben Phung <benphungg@gmail.com>
2020-01-26 22:12:19 -0800

    Add iterm preferences

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ done
 
 mkdir -p ~/.config/karabiner
 ln -s -f $(realpath config/karabiner/karabiner.json) ~/.config/karabiner/karabiner.json
+
 mkdir -p ~/.config/nvim
 ln -s -F $(realpath config/nvim) ~/.config
 ln -s -f ~/.config/nvim/init.vim ~/.vimrc
+
+ln -s -f $(realpath preferences/iterm/com.googlecode.iterm2.plist) ~/Library/Preferences/com.googlecode.iterm2.plist
+
